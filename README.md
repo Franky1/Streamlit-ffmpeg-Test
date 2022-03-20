3dbd03dce316cdc8882f2466aa76d3baa659a598
Franky1 <arvid.teichtmann@gmx.de>
2022-03-20 21:08:21 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Streamlit ffmpeg Test
 
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/franky1/streamlit-ffmpeg-test/main)
+
 Streamlit app to test **ffmpeg** on Streamlit Cloud.
+It uses the `ffmpeg` binary and the python wrapper `ffmpeg-python` library.
+
+## Dependencies
+
+### Binaries in `packages.txt`
+
+```txt
+ffmpeg
+```
+
+### pypi libraries in `requirements.txt`
+
+```txt
+ffmpeg-python
+streamlit
+```
+
+## Status
+
+This is just a quick example project to test ffmpeg on Streamlit Cloud.
+
+> Last changes: 20.03.2022
--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ downloadfile = None
 @st.experimental_memo
 def convert_mp3_to_wav_ffmpeg_bytes2bytes(input_data: bytes) -> bytes:
     """
-    It converts mp3 to wav using ffmpeg.
-
+    It converts mp3 to wav using ffmpeg
     :param input_data: bytes object of a mp3 file
-    :return: a bytes object of a wav file
+    :return: A bytes object of a wav file.
     """
+    # print('convert_mp3_to_wav_ffmpeg_bytes2bytes')
     args = (ffmpeg
             .input('pipe:', format='mp3')
             .output('pipe:', format='wav')
@@ -36,14 +36,23 @@ def on_file_change(uploaded_mp3_file):
     return convert_mp3_to_wav_ffmpeg_bytes2bytes(uploaded_mp3_file.getvalue())
 
 
+def on_change_callback():
+    """
+    It prints a message to the console. Just for testing of callbacks.
+    """
+    print(f'on_change_callback: {uploaded_mp3_file}')
+
+
 # The below code is a simple streamlit web app that allows you to upload an mp3 file
 # and then download the converted wav file.
 if __name__ == '__main__':
     st.title('MP3 to WAV Converter test app')
-    st.markdown('This app converts mp3 to wav using ffmpeg')
-    uploaded_mp3_file = st.file_uploader('Upload Your MP3 File', type=['mp3'])
+    st.markdown("""This is a quick example app for using **ffmpeg** on Streamlit Cloud.
+    It uses the `ffmpeg` binary and the python wrapper `ffmpeg-python` library.""")
 
-    if uploaded_mp3_file is not None:
+    uploaded_mp3_file = st.file_uploader('Upload Your MP3 File', type=['mp3'], on_change=on_change_callback)
+
+    if uploaded_mp3_file:
         uploaded_mp3_file_length = len(uploaded_mp3_file.getvalue())
         filename = pathlib.Path(uploaded_mp3_file.name).stem
         if uploaded_mp3_file_length > 0:
@@ -55,8 +64,9 @@ if __name__ == '__main__':
         length = len(downloadfile)
         if length > 0:
             st.subheader('After conversion to WAV you can download it below')
-            st.download_button(label="Download .wav file",
+            button = st.download_button(label="Download .wav file",
                             data=downloadfile,
                             file_name=f'{filename}.wav',
                             mime='audio/wav')
             st.text(f'Size of "{filename}.wav" file to download: {length} bytes')
+    st.markdown("""---""")
